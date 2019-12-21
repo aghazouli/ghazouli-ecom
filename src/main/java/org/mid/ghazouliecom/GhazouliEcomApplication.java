2e3f1875c66dc35be3299ed736259ba1a7127c3e
aghazouli <ghazouli.abdelhamid@gmail.com>
2019-12-21 12:30:51 +0100

    first-commit

---
+++ src/main/java/org/mid/ghazouliecom/GhazouliEcomApplication.java
@@ -0,0 +1,13 @@
+package org.mid.ghazouliecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhazouliEcomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhazouliEcomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mid/ghazouliecom/GhazouliEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mid.ghazouliecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhazouliEcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
